2f743d996583b3b8d0baf0ce092f7738fb9d67d1
Marcilio Junior <marciliojrs@gmail.com>
2015-11-04 09:39:59 -0200

    Initial Commit

---
+++ HElabsChallenge/AppDelegate/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  HElabsChallenge
+//
+//  Created by Marcilio Junior on 11/4/15
+//  Copyright (c) 2015 HElabs. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
